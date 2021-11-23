0608212b22c8d647c80a6d9d368a8fa903487c63
Vitor Teixeira <vitoroliv@outlook.com>
2021-11-23 11:09:20 -0300

    Controladores e funções

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,46 @@ class CalcIMC extends StatefulWidget {
 }
 
 class _CalcIMCState extends State<CalcIMC> {
+
+  //-- Controladores --//
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  //-! Controladores --//
+
+  String _infoText = "Informe seus dados!";
+
+  //-- Função de Reset --//
+  void _reset () {
+    pesoController.text = "";
+    alturaController.text = "";
+    _infoText= "Informe seus dados!";
+  }
+  //-! Função de Reset --//
+
+  //-- Função calcular --/
+  void _calculate () {
+    setState(() {
+      double peso = double.parse(pesoController.text);   //trasnformando texto em double
+      double altura = double.parse(alturaController.text)/100;
+      double imc = peso / (altura * altura);
+      print(imc);
+      if(imc < 18.6){
+        _infoText= "Abaixo do Peso ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }else if(imc >= 18.6 && imc < 24.9){
+        _infoText= "Peso Ideal ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }else if(imc >= 24.9 && imc < 29.9){
+        _infoText= "Levemente Acima do Peso ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }else if(imc >= 29.9 && imc < 34.9){
+        _infoText= "Obesidade Grau I ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }else if(imc >= 34.9 && imc < 39.9){
+        _infoText= "Obesidade Grau II ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }else if(imc >= 40){
+        _infoText= "Obesidade Grau III ${imc.toStringAsPrecision(3)}"; //3 digitos
+      }
+    });
+  }
+  //-! Função calcular --/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +65,7 @@ class _CalcIMCState extends State<CalcIMC> {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: (_reset),
           ),
         ],
       ),
@@ -36,7 +76,7 @@ class _CalcIMCState extends State<CalcIMC> {
           crossAxisAlignment: CrossAxisAlignment.stretch,  //Alinhando conteúdo da coluna
           children:  <Widget>[
             const Icon(Icons.person_outlined, size: 120.0, color: Colors.deepOrange),
-            const TextField(
+            TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Peso (Kg)",
@@ -44,9 +84,10 @@ class _CalcIMCState extends State<CalcIMC> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
+              controller: pesoController
             ),
 
-            const TextField(
+            TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Altura (cm)",
@@ -54,14 +95,15 @@ class _CalcIMCState extends State<CalcIMC> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
+              controller: alturaController,
             ),
 
             Padding(
-              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0), 
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: SizedBox(
                 height: 50.0,
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed:(_calculate),
                   child: const Text("Calcular",
                     style: TextStyle(color: Colors.white, fontSize: 20.0),),
                   color: Colors.deepOrange,
@@ -69,9 +111,9 @@ class _CalcIMCState extends State<CalcIMC> {
               ),
             ),
 
-            const Text("Info",
+            Text(_infoText,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, color: Colors.deepOrangeAccent),
+              style: const TextStyle(fontSize: 20, color: Colors.deepOrangeAccent),
             ),
 
           ],

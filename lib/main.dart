997946a35a388af53dd21b4e6720167e04bbcf8d
Vitor Teixeira <vitoroliv@outlook.com>
2021-11-23 10:29:25 -0300

    Add Button com espaçamento em ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: CalcIMC(),
     debugShowCheckedModeBanner: false,
   ));
@@ -19,39 +19,63 @@ class _CalcIMCState extends State<CalcIMC> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora IMC"),
+        title: const Text("Calculadora IMC"),
         centerTitle: true,
         backgroundColor: Colors.deepOrange,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.refresh),
+            icon: const Icon(Icons.refresh),
             onPressed: () {},
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const <Widget>[
-          Icon(Icons.person_outlined, size: 120.0, color: Colors.deepOrange),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso (Kg)",
-              labelStyle: TextStyle(color: Colors.deepOrangeAccent),
+
+      body: SingleChildScrollView (  // ScrollView caso a tela precise descer
+        padding: const EdgeInsets.fromLTRB(10.0, 2.0, 10.0, 0.0), //espaçamento
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,  //Alinhando conteúdo da coluna
+          children:  <Widget>[
+            const Icon(Icons.person_outlined, size: 120.0, color: Colors.deepOrange),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(color: Colors.deepOrangeAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura (cm)",
-              labelStyle: TextStyle(color: Colors.deepOrangeAccent),
+
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.deepOrangeAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 25.0),
-          ),
-        ],
+
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0), 
+              child: SizedBox(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: const Text("Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),),
+                  color: Colors.deepOrange,
+                ),
+              ),
+            ),
+
+            const Text("Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, color: Colors.deepOrangeAccent),
+            ),
+
+          ],
+        ),
       ),
     );
   }
